5e5f21abcb7b0795aff59d65f3fd3a6a832548e4
kongminhao <2015141463077@stu.scu.edu.cn>
2018-03-18 10:39:56 +0800

    project start

---
+++ src/components/login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="dialog">
+    <div class="loginPage">
+      <h1>SCUBBS登录</h1>
+      <el-form>
+        <el-form-item label="学号">
+          <el-input type="text" id="user" v-model="formName.user"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input type="password" id="password" v-model="formName.password"></el-input>
+        </el-form-item>
+        <el-button type="primary" @click="submitForm('formName')">提交</el-button>
+        <el-button @click="resetForm">重置</el-button>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+  import Axios from 'axios'
+  export default {
+    name: '',
+    data() {
+      return {
+        formName: {//表单中的参数
+          user: '',
+          userError: '',
+          password: '',
+          passwordError: '',
+          beDisabled: true
+        },
+      }
+    },
+    methods: {
+      submitForm(){
+        Axios.post('',{zjh: this.formName.user, mm: this.formName.password}).then(function(responce){
+        // todo: $store 存储内容
+        })
+      },
+      checkForm(){
+
+      },
+      resetForm(){
+        this.formName.user = '';
+        this.formName.password=''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  html,body{
+    margin: 0;
+    padding: 0;
+    position: relative;
+  }
+  .dialog{
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,.8);
+  }
+  .loginPage{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -150px;
+    margin-left: -175px;
+    width: 350px;
+    min-height: 300px;
+    padding: 30px 20px 20px;
+    border-radius: 8px;
+    box-sizing: border-box;
+    background-color: #fff;
+  }
+  .loginPage p{
+    color: red;
+    text-align: left;
+  }
+</style>
---
+++ src/store/modules/info.js
@@ -0,0 +1,11 @@
+const user = {
+    state: {
+      name: '', // 用户名字
+      nickname: '',// Nickname
+      college: '', // 用户学院
+      cookies:'', // cookies
+      avatar: '', // 头像地址
+      introduction: '', // 简介
+      roles: [], // 角色
+    }
+}
