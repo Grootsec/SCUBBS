dffaf73da349e3bf589f28f4cb83cf2f9aae58b4
Lightning <lightning-zgc@hotmail.com>
2018-03-19 15:43:41 +0800

    Project firest run

--- src/main/java/org/grootsec/scubbs/im/ImApplication.java
+++ src/main/java/org/grootsec/scubbs/im/ImApplication.java
@@ -2,11 +2,20 @@ package org.grootsec.scubbs.im;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, RedisAutoConfiguration.class})
 public class ImApplication {
+    @RequestMapping("/")
+    String index() {
+        return "IM Core of SCUBBS";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ImApplication.class, args);
+    }
 }
