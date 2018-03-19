4349e70ae1df87d95deb495d6bf554d23a3b1098
kongminhao <2015141463077@stu.scu.edu.cn>
2018-03-19 14:43:39 +0800

    产品亲自上阵“

--- src/components/textedit/TextEdit.vue
+++ src/components/textedit/TextEdit.vue
@@ -1,18 +1,22 @@
 <template>
-<div>
-  <Icon type="at"></Icon>
-  <Input v-model="postcontent" type="textarea" :rows="4" placeholder="Enter something..."></Input>
-</div>
+  <div>
+
+
+    <Input v-model="postcontent" type="textarea" :autosize="{minRows: 4}" placeholder="Enter something..."></Input>
+    <Button type="primary" style="    position: relative;
+    top: -18px;
+    left: 10px;" shape="circle" icon="at" v-on:click=""></Button>
+  </div>
 </template>
 
 <script>
-export default {
-  data(){
-    return {
-      postcontent: "",
+  export default {
+    data() {
+      return {
+        postcontent: "",
+      }
     }
   }
-}
 </script>
 
 <style scoped>
