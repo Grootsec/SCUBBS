a4fbcba22d1b271345b49868268e3289a2193ebd
kongminhao <2015141463077@stu.scu.edu.cn>
2018-03-19 14:29:58 +0800

    修改申请界面

--- src/components/applyorganization.vue
+++ src/components/applyorganization.vue
@@ -8,18 +8,36 @@
         <Content :style="{padding: '0 50px'}">
           <Card>
             <div>
-              <label> 组织名称&nbsp;&nbsp;&nbsp;</label>
-              <Input v-model="OrganizationName" placeholder="Enter something..." clearable style="width: 200px"></Input>
+              <Row type="flex" justify="center" class="code-row-bg">
+                <Col span="4">
+                <label> 组织名称&nbsp;&nbsp;&nbsp;</label></Col>
+                <Col span="4">
+                <Input v-model="OrganizationName" placeholder="Enter something..." clearable
+                       style="width: 200px"></Input></Col>
+              </Row>
+            </div>
+            <Row type="flex" justify="center" class="code-row-bg">
+              <Col span="4">
+              <label> 负责人名字</label></Col>
+              <Col span="4">
+              <Input v-model="OrganizationHeadName" placeholder="Enter something..." clearable
+                     style="width: 200px"></Input></Col>
+            </Row>
+            <div>
+              <Row type="flex" justify="center" class="code-row-bg">
+                <Col span="4">
+                <label> 组织所属学院</label> </Col>
+                <Col span="4">
+                <Input v-model="College" placeholder="Enter something..." clearable style="width: 200px"></Input></Col>
+              </Row>
             </div>
             <div>
-              <label> 负责人名字</label>
-              <Input v-model="OrganizationHeadName" placeholder="Enter something..." clearable style="width: 200px"></Input>
+              <Row type="flex" justify="center" class="code-row-bg">
+                <Col span="4">
+              <Button type="success" size="large" style="width: 80%" v-on:click="submit">申请</Button>
+                </Col>
+              </Row>
             </div>
-            <div>
-              <label> 组织所属学院</label>
-              <Input v-model="College" placeholder="Enter something..." clearable style="width: 200px"></Input>
-            </div>
-            <Button type="success"  v-on:click="submit">SUBMIT</Button>
           </Card>
         </Content>
         <Footer class="layout-footer-center">2018 &copy; <a href="http://grootsec.org">grootsec.org</a></Footer>
@@ -54,6 +72,10 @@
 
 
 <style scoped>
+  label {
+    text-align: center;
+    font-size: 20px;
+  }
   .layout {
     border: 1px solid #d7dde4;
     background: #f5f7f9;
